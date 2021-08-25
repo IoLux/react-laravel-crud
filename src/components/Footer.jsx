cd979e7137b843930f9093084f979a3c66dcb6bd
IoLux <yuriaan856@gmail.com>
2021-08-25 10:50:22 +0700

    de

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,38 +3,36 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="wow" style={{ color: "white", backgroundColor: "blue" }}>
-      <footer className="py-2 my-4 mb-0">
-        <ul className="nav justify-content-center border-bottom pb-3 mb-3">
-          <li className="nav-item">
-            <a href="/#" className="nav-link px-2 text-muted">
-              Home
-            </a>
-          </li>
-          <li className="nav-item">
-            <a href="/#" className="nav-link px-2 text-muted">
-              Features
-            </a>
-          </li>
-          <li className="nav-item">
-            <a href="/#" className="nav-link px-2 text-muted">
-              Pricing
-            </a>
-          </li>
-          <li className="nav-item">
-            <a href="/#" className="nav-link px-2 text-muted">
-              FAQs
-            </a>
-          </li>
-          <li className="nav-item">
-            <a href="/#about" className="nav-link px-2 text-muted">
-              About
-            </a>
-          </li>
-        </ul>
-        <p className="text-center text-muted">&copy; 2021 Company, Inc</p>
-      </footer>
-    </div>
+    <footer className="py-2 my-4 mb-0 bg-primary">
+      <ul className="nav justify-content-center border-bottom pb-3 mb-3">
+        <li className="nav-item">
+          <a href="/#" className="nav-link px-2">
+            Home
+          </a>
+        </li>
+        <li className="nav-item">
+          <a href="/#" className="nav-link px-2">
+            Features
+          </a>
+        </li>
+        <li className="nav-item">
+          <a href="/#" className="nav-link px-2">
+            Pricing
+          </a>
+        </li>
+        <li className="nav-item">
+          <a href="/#" className="nav-link px-2">
+            FAQs
+          </a>
+        </li>
+        <li className="nav-item">
+          <a href="/#about" className="nav-link px-2">
+            About
+          </a>
+        </li>
+      </ul>
+      <p className="text-center text-white">&copy; 2021 Company, Inc</p>
+    </footer>
   );
 };
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const Header = () => {
   return (
     <div>
-      <nav className="navbar navbar-expand-lg navbar-dark bg-primary" data-aos="fade-down">
+      <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
         <div className="container-fluid">
           <a className="navbar-brand" href="/">
             Navbar
